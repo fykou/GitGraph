c3ee1b1774df8cc3125f263093fb1b6e38543de3
Marius Arhaug <mariuhar@stud.ntnu.no>
2021-09-10 14:59:07 +0000

    FEAT: add basic boilerplate to homepage

---
+++ src/Main.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import React from 'react'
+import './styles/global.css'
+
+import { Content } from './content/fromAPI'
+
+export default function Main() {
+  return (
+    <div className="Main">
+      <Content />
+    </div>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Header() {
+  return (
+    <header>
+      <h1 className="text-center text-5xl py-16">"Placeholder title"</h1>
+    </header>
+  )
+}

0d7562074ce4f1c82fbbc2da7830b74649fe6b95
Hoeyby <alex.hoeyby@gmail.com>
2021-09-15 10:28:12 +0200

    Added axios

---
+++ documentation/README.md
@@ -0,0 +1,3 @@
+Why Axios?
+- Automatic JSON stringification when sending requests -> cleaner code. A feature not supported by the native Fetch API.
+- Native protection against cross-site request forgery (CSFR) attacks.
--- src/content/fromAPI.tsx
+++ src/content/fromAPI.tsx
@@ -1,51 +1,11 @@
 import React from 'react'
+import axios from 'axios'
 
 export function Content() {
   return (
     <div className="w-3/6 m-auto text-center">
       <p>
-        Sodales interdum dictum dui dis potenti parturient platea facilisi
-        nostra ornare duis. Sociosqu semper vestibulum nunc et litora class
-        laoreet nostra ipsum tempor. Consequat faucibus massa magna lacus lacus
-        ligula euismod ultrices. Fusce torquent magnis quisque platea interdum.
-        Etiam eu senectus tristique porta adipiscing. Scelerisque vulputate nibh
-        taciti placerat augue fermentum semper fames placerat. Iaculis tristique
-        euismod sagittis dolor dictum accumsan porttitor sed viverra egestas
-        iaculis mauris. Tincidunt pulvinar senectus.
-      </p>
-      <p>
-        Sem scelerisque felis cursus convallis libero vehicula duis ipsum
-        tincidunt at, mus praesent. Ligula curae; semper amet augue eros
-        nascetur imperdiet, sapien arcu id varius. Suspendisse placerat
-        dignissim parturient ipsum bibendum facilisi integer dictumst. At nulla
-        hac taciti? Eros consectetur facilisi lobortis. Curabitur sociosqu sed
-        feugiat. Pretium, vivamus parturient lacus. Id taciti quisque et donec
-        congue feugiat! Nulla donec mollis lacus in volutpat ut id enim!
-        Vestibulum dictumst sapien feugiat curae; dictumst nam orci. Sodales
-        tempus purus magnis aliquam congue! Taciti arcu scelerisque aenean cum
-        taciti taciti conubia. Himenaeos integer nulla faucibus quisque semper
-        nec dapibus.
-      </p>
-      <p>
-        Pellentesque nibh ipsum vitae aliquam; libero justo vel mus tempor
-        semper montes adipiscing. Quam etiam semper nam donec pharetra phasellus
-        potenti. Facilisis eu diam tortor accumsan sed tempus in praesent? Eros
-        at sodales bibendum imperdiet proin aenean. Potenti, eget ultricies
-        magnis neque penatibus! Sociis nostra integer parturient conubia mauris
-        sociosqu fusce eu mattis bibendum dolor. Neque eget laoreet volutpat.
-        Hendrerit gravida risus vel ornare placerat blandit elementum fringilla
-        cursus placerat bibendum. Phasellus mattis, porta conubia dapibus.
-      </p>
-      <p>
-        Consequat, malesuada molestie senectus porta mus blandit hendrerit proin
-        sed. Mus augue fermentum dictumst sociis urna tempor, magnis diam
-        venenatis mattis. Erat rutrum vestibulum penatibus class nisi eleifend
-        ad ut odio, felis curae;. Ut lorem in, lacus condimentum sed. Mollis
-        nulla rutrum vulputate? Est inceptos; malesuada lacinia aenean. Tortor
-        senectus, litora neque vehicula habitasse nulla nec semper. Blandit
-        interdum netus sed molestie duis vel, cubilia litora at. Convallis ut
-        cum velit tempor eleifend sed bibendum dignissim. Lobortis nec curae;
-        laoreet et donec neque.
+       
       </p>
     </div>
   )
